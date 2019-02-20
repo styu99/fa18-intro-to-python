0c5c846bb70996aca2da953439dac5ef22c533cb
Claire Zheng <claire.zheng@utexas.edu>
2019-02-19 18:39:15 -0600

    Week 1 Exercises with Answers

---
+++ 2-19-19_Week_1/exercises_print.py
@@ -0,0 +1,29 @@
+# Exercises with Print Statements:
+# 1. Print 2019 on your screen in two ways.
+print(2019)
+x = 2019
+print(x)
+
+# 2. first_name = "John"
+first_name = "John"
+# a. What is the output for print(first_name)?
+print(first_name) # this will print: John
+# b. Suppose I then change first_name = "Bob". What is the output now?
+first_name = "Bob"
+print(first_name) # this will print: Bob
+
+# 3. calvin = "Mike"
+calvin = "Mike"
+# a. What is the output for print(calvin)?
+print(calvin) # this will print: Mike
+# b. What about print("calvin")?
+print("calvin") # this will print: calvin
+
+# The hard one. :)
+
+var = 18
+x = 12
+var = x
+x = var
+print(x) # prints 12
+print(var) # also prints 12
---
+++ 2-19-19_Week_1/exercises_vars_dataTypes.py
@@ -0,0 +1,20 @@
+# Exercises with Variables and Data Types:
+# 1. Store the value of 17 in a variable.
+variable = 17
+
+# 2. is_raining = True
+is_raining = True
+# a. What data type is is_raining?
+type(is_raining) # this line will return that the variable is a Boolean
+# b. How can I change this to make is_raining to store false?
+is_raining = False
+
+# 3. x = 101
+x = 101
+# a. Suppose I have a variable y = False. Can I set x = y? What is the new data type of x?
+y = False
+x = y
+type(x)
+# b. Now suppose I have a variable var = 17. Can I make x store what var stores? How?
+var = 17
+x = var
