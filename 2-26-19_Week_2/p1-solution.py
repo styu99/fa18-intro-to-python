8d9f6b2f295221c16671cbb2547817f80edee1d6
Sean Yu <seantyu@utexas.edu>
2019-03-05 10:19:19 -0600

    solutions for week 2

---
+++ 2-26-19_Week_2/p1-solution.py
@@ -0,0 +1,15 @@
+import grader
+
+'''
+Week 2: Programming Assignment 1
+
+Store the concatenated message into the variable named "combined_message" using the two 
+provided variables "name" and "message". 
+'''
+
+name = "Sean"
+message = " is an instructor of Intro to Python!"
+combined_message = name + message  # Modify this line only
+
+# You can ignore this line! This is just for testing your code. :)
+grader.grade1(combined_message)
---
+++ 2-26-19_Week_2/p2-solution.py
@@ -0,0 +1,26 @@
+import grader
+
+'''
+Week 2: Programming Assignment 2
+
+The program currently returns an error! Please fix it so that it will return the right thing. You can 
+solve this in two ways. Try both!
+1. Change lines 22 and 23
+2. Change line 25 and 26
+
+sentence_1 should store "Sean scored 4th place at the coding competition!"
+sentence_2 should store "Claire scored 5th place at the coding competition!"
+'''
+
+person_1 = "Sean"
+person_2 = "Claire"
+
+message = " place at the coding competition!"
+fourth = str(4)   # Modify this line for solution 1
+fifth = str(5)    # Modify this line for solution 1
+
+sentence_1 = person_1 + " scored " + str(fourth) + "th" + message   # Modify this line for solution 2
+sentence_2 = person_2 + " scored " + str(fifth) + "th" + message    # Modify this line for solution 2
+
+# You can ignore this line! This is just for testing your code. :)
+grader.grade2(sentence_1, sentence_2)
